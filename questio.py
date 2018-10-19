47677ddac19681a834d8b4f1be791d4c0fc96600
root <root@localhost.localdomain>
2018-10-19 08:41:25 -0400

    test1openpyxl

--- questio.py
+++ questio.py
@@ -1,8 +1,28 @@
-#!/usr/env/ python
+#!/usr/bin/env python3
+
+
 
 import os
+import datetime
+from openpyxl import Workbook
 
-print("Ciao a tutti")
-print("Ciao a me")
 
+cwd = os.getcwd()
 
+wb = Workbook()
+
+# grab the active worksheet
+ws = wb.active
+
+# Data can be assigned directly to cells
+ws['A1'] = 42
+
+# Rows can also be appended
+ws.append([1, 2, 3])
+
+# Python types will automatically be converted
+
+ws['A2'] = datetime.datetime.now()
+
+# Save the file
+wb.save("sample.xlsx")
